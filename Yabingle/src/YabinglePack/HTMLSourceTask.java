aa18c2c39b9db065d4dd26e0da33c2588a9d1f18
Hoshi <ssrockmantt@gmail.com>
2017-07-29 17:13:00 +0800

    Added Engine Type in Page Source

--- Yabingle/src/YabinglePack/HTMLSourceTask.java
+++ Yabingle/src/YabinglePack/HTMLSourceTask.java
@@ -17,11 +17,25 @@ public class HTMLSourceTask extends TaskObject
     private final Consumer<StringBuilder> callback;
     private String url;
     private StringBuilder pageSource;
+    private SeachEngineType engineType;
     
     public HTMLSourceTask(Consumer<StringBuilder> callback, String url) 
     {
         this.callback = callback;
         this.url = url;
+        engineType = SeachEngineType.NO_PREFERENCE;
+        
+    }
+
+    public SeachEngineType getEngineType() {
+        return engineType;
+    }
+    
+    public HTMLSourceTask(Consumer<StringBuilder> callback, String url, SeachEngineType engineType) 
+    {
+        this.callback = callback;
+        this.url = url;
+        this.engineType = engineType;
     }
     
     @Override
